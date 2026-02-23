11b182dd14a12f877be5247a89a6ac0c238a47ab
yusuppsaputra <yusupsaputra935@student.esaunggul.ac.id>
2026-02-23 21:12:26 +0700

    kedua

---
+++ app.js
@@ -0,0 +1,18 @@
+const express   = require('express')
+const app       = express()
+const port      = 3001
+const c_beranda = require('./controller/c_beranda')
+
+
+app.use( express.urlencoded({extended:false}) )
+app.set('view engine', 'ejs')
+app.set('views', './view-html')
+app.use( express.static('public'))
+
+
+app.get('/', c_beranda.index)
+
+
+app.listen(port, ()=>{
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
